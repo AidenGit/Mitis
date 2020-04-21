2c85c18ec44b570b057b296e687a4147f5a6ead4
Aiden Git <aidengit@gmail.com>
2020-04-21 13:38:08 +1000

    Adding basic commands framework etc

--- bot.py
+++ bot.py
@@ -10,4 +10,17 @@ async def on_ready():
     print('Bot is alive')
 
 
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'PONG! Ping to server is: {round(client.latency *1000 )}ms')
+
+
+# @client.event
+# async def on_member_join(member):
+#     print(f'{member} has joined the server.')
+#
+# @client.event
+# async def on_member_remove(member):
+#     print(f'{member} has left the server.')
+
 client.run(token)
